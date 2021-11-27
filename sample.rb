0e78b0c75aa5363e6a70643ee337c4ea674afb82
midorinoyama <fujiaqa@gmail.com>
2021-11-27 08:04:33 +0000

    FINISH2-3 文字列

--- sample.rb
+++ sample.rb
@@ -3,5 +3,10 @@ puts 1 + 2
 a = 'Hello, world'
 puts a
 
-b = 'こんにちわ'
+b = "こんに\nちわ"# \nは改行文字、シングルクォートで囲むとただの文字列
 puts b
+
+name = 'Alice'
+puts "Hello, #{name}!" # ダブルクォートだと式展開#{}が使える
+
+puts 'Hello, ' + name + '!' # 結果は同上、式展開を使った方が分かりやすい
